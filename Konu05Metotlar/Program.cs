97f1035a2938abb45dd141246c48a22bd5757f54
user <user@NEVERMORE>
2026-01-15 21:34:14 +0300

    Add project files.

---
+++ Konu05Metotlar/Program.cs
@@ -0,0 +1,54 @@
+﻿namespace Konu05Metotlar
+{
+    internal class Program
+    {
+        static void ToplamaYap() // metot tanımlama
+        {
+            // void olan metotlar geriye döndürmeyen metotlardır
+            Console.WriteLine(10 + 8);
+
+        }
+        static void Main(string[] args)
+        {
+            ToplamaYap();
+            Console.WriteLine("Konu 05 Metotlar");
+            Console.WriteLine();
+            ToplamaYap(10 , 8);
+            Console.WriteLine();
+            Console.WriteLine("Geriye değer döndüren metot:");
+            int sonuc = ToplamaYap(6, 6, 6);
+            Console.WriteLine("Sayıların Toplamı :" + sonuc);
+            Console.WriteLine();
+            Console.WriteLine("Email giriniz: ");
+            var email = Console.ReadLine();
+            var mailGonderildimi = MailGonder(email);
+            if(mailGonderildimi == true)
+            {
+                Console.WriteLine("Mail Başarıyla gönderildi..");
+            }
+            else
+                Console.WriteLine("Mail Gönderilemedi!");
+
+        }
+        
+        static void ToplamaYap(int sayi1, int sayi2) // parametreli metot tanımlama
+        {
+            Console.WriteLine(sayi1 + sayi2); // methoda gönderilen değerleri topla
+
+        }
+        static int ToplamaYap(int sayi1, int sayi2, int sayi3) // parametreli değer döndüren metot tanımlama
+        {
+            return sayi1 + sayi2 + sayi3; // methoda gönderilen değerleri topla
+        }
+        static bool MailGonder(string mailAdresi)
+        {
+            if (!string.IsNullOrEmpty(mailAdresi))
+            {
+                // burada mail gönderimi yapılabilir
+                return true;
+            }
+            return false;
+        }
+
+    }
+}
