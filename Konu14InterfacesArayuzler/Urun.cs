0aba65884e98ed193f4b5a6137cc2fd0d38244e1
user <user@NEVERMORE>
2026-01-29 22:37:29 +0300

    29-01-26

--- Konu14InterfacesArayuzler/Urun.cs
+++ Konu14InterfacesArayuzler/Urun.cs
@@ -1,10 +1,15 @@
 ﻿namespace Konu14InterfacesArayuzler
 {
-    internal interface ISinifGereksinimleri // bu interface her class ta bulunması gereken zorunlu özellikleri içeriyor.
+    internal class Urun : ISinifGereksinimleri
     {
         public int Id { get; set; } // nesnenin benzersiz kimlik değeri
         public DateTime CreateDate { get; set; } // nesnenin oluşma zamanını tutacak özellik
         public DateTime UpdateDate { get; set; } // nesnenin son güncellenme zamanını tutacak özellik
         public bool IsActive { get; set; } // nesnenin aktiflik durumunu tutacak olan özellik
+        public string Name { get; set; }
+        public string? Description { get; set; } // description = açıklama
+        public string? Image { get; set; }
+        public decimal Price { get; set; }
+        public int Stock { get; set; }
     }
 }
